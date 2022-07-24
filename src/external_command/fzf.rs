0e0b3807f084ea84e0a27df07b86944762917902
Hogeyama <gan13027830@gmail.com>
2022-07-25 00:30:46 +0900

    fix: Initial load

--- src/external_command/fzf.rs
+++ src/external_command/fzf.rs
@@ -23,6 +23,10 @@ pub fn new(myself: impl Into<String>, socket: impl Into<String>) -> Command {
     fzf.args(vec!["--preview-window", "right:50%:noborder"]);
     fzf.args(vec!["--header-lines=1"]);
     fzf.args(vec!["--prompt", "files>"]);
+    fzf.env(
+        "FZF_DEFAULT_COMMAND",
+        format!("{myself} load --socket {socket} -- fd ."),
+    );
     fzf
 }
 
