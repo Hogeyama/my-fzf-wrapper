59605087f75a58693b13bb5812233d7116d281c4
Hogeyama <gan13027830@gmail.com>
2023-02-04 23:48:53 +0900

    fix: Handle errors in server.rs

--- src/server.rs
+++ src/server.rs
@@ -44,30 +44,46 @@ pub async fn server(
                     } = params;
                     mode = config.get_mode(mode_name);
                     let resp = mode.load(&mut state, args).await;
-                    send_response(&mut tx, method, resp).await?;
+                    match send_response(&mut tx, method, resp).await {
+                        Ok(()) => trace!("server: load done"),
+                        Err(e) => error!("server: load error"; "error" => e),
+                    }
                 }
                 Some(method::Request::Preview { method, params }) => {
                     let method::PreviewParam { item } = params;
                     let resp = mode.preview(&mut state, item).await;
-                    send_response(&mut tx, method, resp).await?;
+                    match send_response(&mut tx, method, resp).await {
+                        Ok(()) => trace!("server: preview done"),
+                        Err(e) => error!("server: preview error"; "error" => e),
+                    }
                 }
                 Some(method::Request::Run { method, params }) => {
                     let method::RunParam { item, args } = params;
                     match clap_parse_from(args) {
                         Ok(opts) => {
                             let resp = mode.run(&mut state, item, opts).await;
-                            send_response(&mut tx, method, resp).await?;
+                            match send_response(&mut tx, method, resp).await {
+                                Ok(()) => trace!("server: run done"),
+                                Err(e) => error!("server: run error"; "error" => e),
+                            }
                         }
                         Err(e) => {
                             error!("server: clap parse error"; "error" => e.to_string());
-                            send_response(&mut tx, method, RunResp).await?
+                            match send_response(&mut tx, method, RunResp).await {
+                                Ok(()) => trace!("server: run done"),
+                                Err(e) => error!("server: run error"; "error" => e),
+                            }
                         }
                     }
+                    info!("server: run done");
                 }
                 Some(method::Request::Reload { method, params: () }) => {
                     let args = state.last_load.args.clone();
                     let resp = mode.load(&mut state, args).await;
-                    send_response(&mut tx, method, resp).await?;
+                    match send_response(&mut tx, method, resp).await {
+                        Ok(()) => trace!("server: reload done"),
+                        Err(e) => error!("server: reload error"; "error" => e),
+                    }
                 }
                 _ => {
                     tx.write_all("\"Unknown request\"".as_bytes())
@@ -86,7 +102,7 @@ async fn send_response<M: method::Method, TX: AsyncWriteExt + Unpin>(
     _method: M, // 型合わせ用
     resp: <M as Method>::Response,
 ) -> Result<(), String> {
-    let resp = serde_json::to_string(&resp).expect("nyaan") + "\n";
+    let resp = serde_json::to_string(&resp).unwrap() + "\n";
     tx.write_all(resp.as_bytes())
         .await
         .map_err(|e| e.to_string())?;
