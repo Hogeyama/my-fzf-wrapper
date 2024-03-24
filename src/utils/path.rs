ec6d097d5f0659713d0c7ea43e81f6654daffdb8
Hogeyama <gan13027830@gmail.com>
2024-03-24 15:49:22 +0900

    refactor(utils): Refactor `to_relpath` to use `Path` instead of `str`

--- src/utils/path.rs
+++ src/utils/path.rs
@@ -1,5 +1,13 @@
-pub fn to_relpath(path: impl AsRef<str>) -> String {
-    let pwd = std::env::current_dir().unwrap();
-    let pwd = pwd.to_str().unwrap();
-    path.as_ref().replace(&format!("{pwd}/"), "")
+use std::path::Path;
+
+pub fn to_relpath(path: impl AsRef<Path>) -> String {
+    let current_dir = std::env::current_dir().unwrap_or_default();
+    let stripped_path = match path.as_ref().strip_prefix(&current_dir) {
+        Ok(stripped_path) => stripped_path,
+        Err(_) => path.as_ref(),
+    };
+    stripped_path
+        .to_str()
+        .expect("Invalid UTF-8 path")
+        .to_string()
 }
